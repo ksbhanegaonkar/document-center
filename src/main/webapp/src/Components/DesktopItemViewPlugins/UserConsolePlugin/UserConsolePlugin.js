ededa0d15251a14677612ddbbe7f0dbb21f0e5a1
ksbhanegaonkar <kedarsbhanegaonkar@gmail.com>
2020-04-17 20:29:48 +0530

    Added some implementation to user plugin

--- src/main/webapp/src/Components/DesktopItemViewPlugins/UserConsolePlugin/UserConsolePlugin.js
+++ src/main/webapp/src/Components/DesktopItemViewPlugins/UserConsolePlugin/UserConsolePlugin.js
@@ -3,7 +3,11 @@ import './UserConsolePlugin.css';
 
 class UserConsolePlugin extends Component{
 
-
+    state={
+        userName:"",
+        email:"",
+        successMsg:''
+    }
   componentDidMount(){
     //this.getPayload(this.props.item.appId);
   }
@@ -11,13 +15,33 @@ class UserConsolePlugin extends Component{
 
 
             return (<div className='user-console'>
-         User app
-            </div>);
+                        <label className="user-console-label">User Name :</label>
+                        <input className="user-console-input" type="text" onChange={this.setUserName.bind(this)} placeholder="User Name" name="username" required/>
+                        <br></br>
+                        <label className="user-console-label">Email :</label>
+                        <input className="user-console-input" type="text" onChange={this.setEmail.bind(this)} placeholder="Enter Email" name="email" required/>
+                        <br></br>
+                        <button onClick={this.addUser.bind(this)}>Add User</button>
+                        <br></br>
+    
+                        <div className="user-added-success-message">
+                            <span>{this.state.successMsg}</span>
+                        </div>
+                     </div>);
         }
 
     
+        setUserName(e){
+            this.setState({userName:e.target.value});
+        }
 
+        setEmail(e){
+            this.setState({email:e.target.value});
+        }
 
+        addUser(){
+            this.setState({successMsg:"User added successfully...!"});
+        }
 
 
 }
